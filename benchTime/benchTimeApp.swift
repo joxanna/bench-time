ed3b8c1ba2485e1e2b50659f7e420dc29f24684d
joxanna <joxannaxue@gmail.com>
2024-05-07 15:25:07 +1000

    Initial Commit

---
+++ benchTime/benchTimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  benchTimeApp.swift
+//  benchTime
+//
+//  Created by Joanna Xue on 7/5/2024.
+//
+
+import SwiftUI
+
+@main
+struct benchTimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
